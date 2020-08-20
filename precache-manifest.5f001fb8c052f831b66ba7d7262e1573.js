6f0c7b7e5b660cc0e5ffa6b9eddf58b9b4b4fa3f
unknown <adityatata95@gmail.com>
2020-08-20 16:18:14 +0530

    Updates

---
+++ precache-manifest.5f001fb8c052f831b66ba7d7262e1573.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e9f5c4a15e5da2c35f76b72c43718227",
+    "url": "/spacex_app/index.html"
+  },
+  {
+    "revision": "5519536e53aceb2768ce",
+    "url": "/spacex_app/static/css/2.1357a289.chunk.css"
+  },
+  {
+    "revision": "be9ac95b7cb2314c8591",
+    "url": "/spacex_app/static/css/main.60f59487.chunk.css"
+  },
+  {
+    "revision": "5519536e53aceb2768ce",
+    "url": "/spacex_app/static/js/2.c8322375.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/spacex_app/static/js/2.c8322375.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "be9ac95b7cb2314c8591",
+    "url": "/spacex_app/static/js/main.7793afab.chunk.js"
+  },
+  {
+    "revision": "473749edac7a92bbd9d7",
+    "url": "/spacex_app/static/js/runtime-main.560f9222.js"
+  }
+]);
